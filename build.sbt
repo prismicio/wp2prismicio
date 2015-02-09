10d08e56489a510ed63b801bc576bb322ceb4473
Sebastien Renault <sre@zenexity.com>
2015-02-09 15:30:21 +0100

    Parse content to identify spans

--- build.sbt
+++ build.sbt
@@ -8,5 +8,5 @@ libraryDependencies ++= Seq(
   "org.scala-lang.modules" %% "scala-xml" % "1.0.2",
   "com.typesafe.play" %% "play-json" % "2.4.0-M2",
   "com.typesafe.play" %% "play-json" % "2.4.0-M2",
-  "org.jsoup" % "jsoup" % "1.8.1"
+  "net.htmlparser.jericho" % "jericho-html" % "3.3"
 )
--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -1,22 +1,30 @@
 import scala.collection.JavaConversions._
 import scala.xml.XML
-import org.jsoup._
-import org.jsoup.nodes.Element
+import net.htmlparser.jericho._
+// import org.jsoup._
+// import org.jsoup.nodes.Element
 import org.lorecraft.phparser.SerializedPhpParser
 import java.util.{ Date, TimeZone }
 import java.text.SimpleDateFormat
 import scala.collection.mutable.{ Map => MMap }
-import play.api.libs.json.{ JsValue, Json, JsArray }
+import play.api.libs.json.{ JsValue, Json, JsArray, JsNull }
+import java.net.URL
 
 object Wp2Prismic {
 
   // WORDPRESS
 
-  case class WPost(id: String, slug: String, title: String, description: String, content: String, categories: List[String], tags: List[String], author: String, at: Date, comment: Boolean, image: Option[WPImage])
+  case class WPost(id: String, slug: String, title: String, description: String, content: String, categories: List[WPCategory], tags: List[String], author: WPAuthor, at: Date, comment: Boolean, image: Option[WPImage])
 
   type WParagraph = String
 
-  case class WPImage(id: String, url: String, width: Option[Int], height: Option[Int], credit: Option[String], copyright: Option[String], alt: Option[String], thumnails: Seq[WPImage])
+  case class WPImage(id: String, url: String, width: Option[Int], height: Option[Int], credit: Option[String], copyright: Option[String], alt: Option[String], thumbnails: Seq[WPImage])
+
+  case class WPAuthor(login: String, firstName: String, lastName: String) {
+    lazy val fullName = firstName + " " + lastName
+  }
+
+  type WPCategory = String
 
   object WPImage {
     def apply(id: String, url: String, alt: Option[String], description: Option[String], data: String): WPImage = {
@@ -35,7 +43,6 @@ object Wp2Prismic {
         val x: MMap[String, Any] = m.asInstanceOf[java.util.LinkedHashMap[String, Any]]
         x
       } getOrElse MMap.empty
-
       val thumbnails = Seq("thumbnail", "medium", "post-thumbnail").flatMap { size =>
         sizes.get(size).flatMap { t =>
           val x: MMap[String, Any] = t.asInstanceOf[java.util.LinkedHashMap[String, Any]]
@@ -45,7 +52,7 @@ object Wp2Prismic {
             val thumbnailUrl = {
               url.split('/').init.mkString("/") + "/" + file
             }
-            WPImage(id + "_" + file, thumbnailUrl, width, height, credit, copyright, alt, Seq.empty)
+            WPImage(size, thumbnailUrl, width, height, credit, copyright, alt, Seq.empty)
           }
         }
       }
@@ -53,84 +60,146 @@ object Wp2Prismic {
     }
   }
 
-  case class WPAuthor(login: String, firstName: String, lastName: String) {
-    lazy val fullName = firstName + " " + lastName
+  // PRISMIC
+  case class Block(text: String, spans: List[Span], openTags: List[(Tag, Int)]) {
+    def append(t: String): Block = Block(text + t, spans, openTags)
+    def append(c: Char): Block = Block(text + c, spans, openTags)
   }
 
-  // PRISMIC
-  object StructuredText {
-
-    def apply(text: String, spans: JsArray): JsValue =
+  object Block {
+    def apply(): Block = Block("", Nil, Nil)
+    def toJson(block: Block): JsValue =
       Json.obj(
         "type" -> "paragraph",
         "content" -> Json.obj(
-          "text" -> text,
-          "spans" -> spans
+          "spans" -> block.spans.map(Span.toJson),
+          "text" -> block.text
         )
       )
+  }
 
-    def strong(start: Int, end: Int) =
-      Json.obj("start" -> start, "end" -> end, "type" -> "strong")
+  case class Span(start: Int, end: Int, typ: String, data: JsValue)
 
-    def em(start: Int, end: Int) =
-      Json.obj("start" -> start, "end" -> end, "type" -> "strong")
+  object Span {
 
-    def hyperlink(start: Int, end: Int, url: String) =
+    def toJson(span: Span): JsValue =
       Json.obj(
-        "start" -> start,
-        "end" -> end,
-        "type" -> "hyperlink",
-        "data" -> Json.obj("url" -> url)
+        "start" -> span.start,
+        "end" -> span.end,
+        "type" -> span.typ,
+        "data" -> span.data
       )
 
-    def label(start: Int, end: Int, data: String) =
-      Json.obj(
-        "start" -> start,
-        "end" -> end,
-        "type" -> "label",
-        "data" -> data
-      )
+    def strong(start: Int, end: Int): Span =
+      Span(start, end, "strong", JsNull)
 
-    def quote(start: Int, end: Int) =
-      label(start, end, "quote")
+    def em(start: Int, end: Int): Span =
+      Span(start, end, "em", JsNull)
 
-    def del(start: Int, end: Int) =
-      label(start, end, "del")
+    def hyperlink(start: Int, end: Int, url: String): Span =
+      Span(start, end, "hyperlink", Json.obj("url" -> url))
 
-    def ins(start: Int, end: Int) =
-      label(start, end, "ins")
+    def label(start: Int, end: Int, data: String): Span =
+      Span(start, end, "label", Json.obj("data" -> data))
 
-    def img(start: Int, end: Int, width: Int, height: Int, url: String) =
-     Json.obj(
-        "start" -> start,
-        "end" -> end,
-        "type" -> "image",
-        "data" -> Json.obj(
-          "origin" -> Json.obj(
-            "height" -> height,
-            "width" -> width,
-            "url" -> url
-          )
+    def quote(start: Int, end: Int): Span =
+      Span(start, end, "quote", JsNull)
+
+    def del(start: Int, end: Int): Span =
+      Span(start, end, "del", JsNull)
+
+    def ins(start: Int, end: Int): Span =
+      Span(start, end, "ins", JsNull)
+
+    def img(start: Int, end: Int, width: Int, height: Int, url: String): Span =
+      Span(start, end, "image", Json.obj(
+        "origin" -> Json.obj(
+          "height" -> height,
+          "width" -> width,
+          "url" -> url
         )
-      )
+      ))
   }
 
   object BlogPost {
 
-    def apply(slug: String, title: String, body: JsValue, shortlede: JsValue, image: JsValue, author: JsValue, date: JsValue, categories: Seq[JsValue], comment: Boolean): JsValue =
+    def description(content: String): JsValue = {
+      // Identify only paragraph here.
+      Json.obj()
+    }
+
+    def image(data: Option[WPImage]): JsValue = {
+      data.map { d =>
+        Json.obj(
+          "alt" -> d.alt,
+          "credits" -> d.credit,
+          "origin" -> Json.obj(
+            "height" -> d.height,
+            "width" -> d.width,
+            "url" -> d.url
+          ),
+          "thumbnails" -> d.thumbnails.map { thumbnail =>
+            val key = thumbnail.id match {
+              case "thumbnail" => "thumbnail"
+              case "medium" => "medium"
+              //case "large" =>
+              case _ => ""
+            }
+            Json.obj(
+              key -> Json.obj(
+                "alt" -> thumbnail.alt,
+                "credits" -> thumbnail.credit,
+                "origin" -> Json.obj(
+                  "height" -> thumbnail.height,
+                  "width" -> thumbnail.width,
+                  "url" -> thumbnail.url
+                )
+              )
+            )
+          }
+        )
+      } getOrElse JsNull
+    }
+
+    def date(at: Date): JsValue = {
+      Json.obj()
+    }
+
+    def body(content: String): JsValue = {
+      val paragraphs = content.split("\n\n").foldRight(Seq.empty[String]) { (p, acc) =>
+        ("<p>" + p + "</p>") +: acc
+      }
+
+      val blocks = paragraphs.foldLeft(List.empty[Block]) { (blocks, paragraph) =>
+        val source = new Source(paragraph)
+        wpParagraph2Block(source.getFirstElement) +: blocks
+      }
+
+      blocks.zipWithIndex.foldLeft(Json.obj()) {
+        case (acc, (block, index)) =>
+          Json.obj(index.toString -> Block.toJson(block))
+      }
+    }
+
+    def author(someone: WPAuthor): JsValue = {
+      Json.obj()
+    }
+
+    def categories(categories: Seq[WPCategory]): JsValue = {
+      Json.arr()
+    }
+
+    def apply(wpost: WPost): JsValue =
       Json.obj(
-        "uid" -> slug,
-        "title" -> title,
-        "body" -> body,
-        "shortlede" -> shortlede,
-        "image" -> image,
-        "date" -> date,
-        "author" -> author,
-        "categories" -> categories.zipWithIndex.foldLeft(Json.obj()) {
-          case (acc, (category, index)) =>
-            acc ++ Json.obj(index.toString -> category)
-        },
-        "allow_comments" -> comment
+        "uid" -> wpost.slug,
+        "title" -> wpost.title,
+        "body" -> BlogPost.body(wpost.content),
+        "shortlede" -> BlogPost.description(wpost.description),
+        "image" -> BlogPost.image(wpost.image),
+        "date" -> BlogPost.date(wpost.at),
+        "author" -> BlogPost.author(wpost.author),
+        "categories" -> BlogPost.categories(wpost.categories),
+        "allow_comments" -> wpost.comment
       )
   }
 
@@ -176,7 +245,7 @@ object Wp2Prismic {
       val content = (p \ "encoded").filter(_.prefix == "content").text
       val author = {
         val login = (p \ "creator").text
-        authors.find(_.login == login) map (_.fullName) getOrElse login
+        authors.find(_.login == login) getOrElse sys.error("oops")
       }
       val tagsAndCategories = (p \\ "category")
       val categories = tagsAndCategories.filter(_.attribute("domain").exists(x => x.toString == "category")).flatMap(_.attribute("nicename").map(_.toString)).toList
@@ -204,21 +273,22 @@ object Wp2Prismic {
     }
   }
 
+  private def wpParagraph2Block(paragraph: Element): Block = {
+    val excluded = List("<p>", "</p>")
+    paragraph.getNodeIterator.toList.foldLeft(Block()) {
+      case (block, tag:Tag) if excluded.contains(tag.tidy) => block
+      case (block, tag:StartTag) =>
+        block.copy(openTags = (tag, block.text.size) +: block.openTags)
+      case (block, tag:EndTag) =>
+        val (openTag, start) = block.openTags.head
+        val span = Span(start, block.text.size, openTag.tidy, Json.obj())
+        block.copy(openTags = block.openTags.tail, spans = span +: block.spans)
+      case (block, c:CharacterReference) => block append c.getChar
+      case (block, text) => block append text.toString
+    }
+  }
+
   private def wp2prismic(wpost: WPost) = {
-    val content = wpost.content.split("\n\n").foldLeft("") { (acc, p) =>
-      acc + "<p>" + p + "</p>"
-    }
-
-    val root = Jsoup.parse(content).body
-
-    def step(el: Element, cursor: Int, level: Int): JsValue = {
-      root.childNodes.foldLeft(Json.obj()) { (acc, el) => //PARAGRAPH LEVEL
-        //println("------------>")
-        //println(el.acc)
-        acc
-      }
-    }
-
-    step(root, 0, 0)
+    BlogPost(wpost)
   }
 }
